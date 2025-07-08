8dfa0906d6188538bf0d65bcf362c4e5f5f0cbe7
HusnainDot <husnainmaroof95@gmail.com>
2025-07-08 17:33:30 +0500

    fixed content with animation done

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Fixed from "./Components/Fixed";
 
 const Home = () => {
+  let [scrollPercent, setScrollPercent] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const docHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+      const scrolled = (scrollTop / docHeight) * 100;
+      setScrollPercent(scrolled);
+    };
+
+
+    
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  
   return (
     <>
       <div className="">
-        {" "}
-        <Fixed />
-       </div>
+        <Fixed scrollPercent={scrollPercent} />
+
+        <div className="w-full h-screen bg-white "></div>
+        <div className="w-full h-screen bg-cyan-400 "></div>
+        <div className="w-full h-screen bg-teal-400 "></div>
+        <div className="w-full h-screen bg-green-400 "></div>
+      </div>
     </>
   );
 };
